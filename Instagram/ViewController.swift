052aff37d2c2a02db4d3095d12e2ccf655e7e6f5
BoltayevFeruz <boltayevferuz777@gmail.com>
2022-07-16 19:42:04 +0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Feruz Boltayev on 16.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
